565f270eaad5a67ea7d202c5cee955d1602db6aa
zhangtao25 <37892968+zhangtao25@users.noreply.github.com>
2019-12-13 16:23:01 +0800

    upd

--- src/layouts/app-tab-bar.vue
+++ src/layouts/app-tab-bar.vue
@@ -2,7 +2,8 @@
   <div class="app-tab-bar">
     <ul>
       <li v-for="(item,index) of items" :key="index" :class="{actived:item.name === current}" @click="goTo(item.name)">
-        <span v-if="item.name !== 3">{{item.label}}</span>
+<!--        <span v-if="item.name !== 3">{{item.label}}</span>-->
+        <i class="icon-tab-bar" v-if="item.name !== 3"></i>
         <img v-if="item.name === 3" class="add" src="./../assets/images/add.png" alt="">
       </li>
     </ul>
@@ -30,17 +31,17 @@
     methods:{
       goTo(name){
         this.current = name
-        if (name === 1){
-          this.$router.push({path:'/home'})
-        } else if (name === 2){
-          this.$router.push({path:'/mall'})
-        } else if (name === 3){
-          this.$router.push({path:'/add'})
-        } else if (name === 4){
-          this.$router.push({path:'/msg'})
-        } else if (name === 5){
-          this.$router.push({path:'/mine'})
-        }
+        // if (name === 1){
+        //   this.$router.push({path:'/home'})
+        // } else if (name === 2){
+        //   this.$router.push({path:'/mall'})
+        // } else if (name === 3){
+        //   this.$router.push({path:'/add'})
+        // } else if (name === 4){
+        //   this.$router.push({path:'/msg'})
+        // } else if (name === 5){
+        //   this.$router.push({path:'/mine'})
+        // }
       }
     }
   }
@@ -66,10 +67,56 @@
       display: flex;
       align-items: center;
       justify-content: center;
+      i.icon-tab-bar{
+        width: 40px;
+        height: 40px;
+        display: block;
+        background-color: antiquewhite;
+        background-size: auto 80px;
+        background-image: url("./../assets/images/icon-tab-bar.png");
+      }
     }
-    li.actived{
-      color: #333;
-      font-size: 32px;
+    li:nth-child(1){
+      i.icon-tab-bar{
+
+      }
+    }
+    li:nth-child(2){
+      i.icon-tab-bar{
+        background-position: -40px 0;
+      }
+    }
+
+    li:nth-child(4){
+      i.icon-tab-bar{
+        background-position: -80px 0;
+      }
+    }
+    li:nth-child(5){
+      i.icon-tab-bar{
+        background-position: -120px 0;
+      }
+    }
+
+    li:nth-child(1).actived{
+      i.icon-tab-bar{
+        background-position: 0 40px;
+      }
+    }
+    li:nth-child(2).actived{
+      i.icon-tab-bar{
+        background-position: -40px 40px;
+      }
+    }
+    li:nth-child(4).actived{
+      i.icon-tab-bar{
+        background-position: -80px 40px;
+      }
+    }
+    li:nth-child(5).actived{
+      i.icon-tab-bar{
+        background-position: -120px 40px;
+      }
     }
     .add{
       width: 92px;
